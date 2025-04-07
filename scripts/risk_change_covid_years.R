9b8546289810e3927f5493391a3b43638f376bb0
ldg-tamas-szabo <tamas.szabo@merck.com>
2025-04-07 14:51:55 +0200

    First implementation of the yearly impact estimate

--- scripts/risk_change_covid_years.R
+++ scripts/risk_change_covid_years.R
@@ -74,3 +74,88 @@ calculate_risk_difference <- function(in_tab){
     ) %>%
     purrr::pmap_dfr(.errorprone_rr_for_stratum)
 }
+
+
+#' Calculate the estimated impact and the Confidence Interval from a Poisson-model
+#'
+#' @param calculated_model A poisson model fit using `glm`.
+#' @param stub_df A dataframe with unique grouping variables in the input. Output columns will be appended.
+#' @return A dataframe with grouping variables and the calcualted estimated value, CI_lo, CI_hi and p-value.
+#' @examples
+#' add_impact_CI_from_model(calculated_model, stub_df)
+  .add_impact_CI_from_model <- function(
+    calculated_model, stub_df, mid_var="estimate"
+) {
+  
+  #TODO: extract estimate for year dummies, not the timeline
+
+  # Use the robust method in the sandwich package to estimate significance
+  result_estimate_p <- calculated_model %>%
+    lmtest::coeftest(vcov = sandwich::sandwich) %>%
+    .[2,] %>% 
+    data.frame() %>%
+    t()
+
+  # Use the robust method in the sandwich package to estimate CIs
+  ci_df <- calculated_model %>%
+    lmtest::coefci(vcov = sandwich::sandwich) %>%
+    data.frame() %>%
+    slice(2)
+  
+  # Add calculated values to a stub of the input
+  stub_df[["impact"]] <- result_estimate_p[1]
+  stub_df$CI_lo <- ci_df[[1]]
+  stub_df$CI_hi <- ci_df[[2]]
+  stub_df$p_value <- result_estimate_p[4]
+    
+  stub_df
+}
+
+
+#' @param in_tab Input dataframe, containing case numbers, population size and time informaiton.
+#' @param grouping_vars  The columns that identify strata, in addtion to Age and Sex.
+#' @param base_min Starting year (time) of the period to use for regression.
+#' @param base_max End year (time) of the period to use for regression.
+#' @param impacted_years Years, where the impact (difference) is estimated.
+#' @return A dataframe with grouping variables and an estimate of the impact for the given period(s).
+#' @examples
+#' calculate_impact_of_year("Diagnosis", 2011, 2019)
+calculate_impact_of_year <- function(
+    in_tab, grouping_vars, base_min=2011, base_max=2019,
+    impacted_years = c(2020, 2021), ...
+) {
+  model_input <- in_tab %>%
+    mutate(
+      N_cases  = round(N_cases, 0),
+      Period   = factor(Period),
+      Age      = as.numeric(factor(Age)),
+      logpop   = log(Population)
+    )
+  
+  for (PR_year in impacted_years) {
+    model_input[[paste("Period", PR_year, sep="_")]] <- ifelse(
+      model_input$Period == PR_year, 1, 0
+    )
+  }
+  
+  custom_formula <- "N_cases ~ Period + Age + offset(logpop)" %>%
+    c(paste("Period", impacted_years, sep="_")) %>%
+    paste(collapse = " + ") %>%
+    as.formula()
+  
+  model <- model_input %>%
+    filter(Period %in% seq(base_min, base_max)) %>%
+    mutate(
+      Period = as.numeric(Period)
+    ) %>%
+    glm(
+      custom_formula, family = poisson(link = "log"),
+      data = ., ...
+    )
+  
+  # Extract information from model
+  .add_impact_CI_from_model(
+      model, distinct(in_tab[, grouping_vars])
+    )
+
+}
