d387c3a5cbb0f8a0dab257718e9354bb152295ed
szabogtamas <szabogtamas@gmail.com>
2025-04-26 20:31:41 +0200

    Added test for esp weight getter

--- tests/test_standardized_rates.R
+++ tests/test_standardized_rates.R
@@ -4,6 +4,39 @@
 
 source("../scripts/standardized_rates.R", chdir=TRUE)
 
+# Testing helpers
+
+example_esp_tab <- get_esp_pop(2013, breaks = seq(40, 70, 10))
+test_that(
+  "ESP2013 weights",
+  expect_equal(example_esp_tab$Std_size, c(47000, 14000, 13500, 11500, 14000))
+)
+test_that(
+  "Column names by default",
+  expect_equal(colnames(example_esp_tab), c("Std_size", "Age"))
+)
+
+example_esp_tab <- get_esp_pop(
+  2013, breaks = seq(0, 90, 10), age_column = "A1", pop_column = "B2"
+)
+test_that(
+  "ESP2013 weights wider",
+  expect_equal(example_esp_tab$B2, c(10500, 11000, 12000, 13500, 14000, 13500, 11500, 9000, 4000, 1000))
+)
+test_that(
+  "Column names customized",
+  expect_equal(colnames(example_esp_tab), c("A1", "B2"))
+)
+
+example_esp_tab <- get_esp_pop(1976, breaks = seq(0, 90, 10))
+test_that(
+  "ESP1976 weights",
+  expect_equal(example_esp_tab$Std_size, c(15000, 14000, 14000, 14000, 14000, 13000, 9000, 5000, 1600, 400))
+)
+
+
+# Tests to ensure consistency with previous results
+
 standardized_table <- example_dataset %>%
   calculate_standardized_rate(
     extra_grouping_vars="Diagnosis",
